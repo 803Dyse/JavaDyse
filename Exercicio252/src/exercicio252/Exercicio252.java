a86a2561a486a62ba4a02b04a8c9379ce3d6827a
DyseHousePC <>
2022-10-16 19:55:00 +0200

    first and probably last commit of these five projects, simple thing

---
+++ Exercicio252/src/exercicio252/Exercicio252.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio252;
+
+/**
+ *
+ * @author Bilo Alejandro Martins Gonzalez
+ */
+public class Exercicio252 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 7) {
+            System.out.println("Ola curso");
+            x++;
+        }
+
+    }
+
+}
