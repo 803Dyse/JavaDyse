d804c1fb45ad516439b47d1d95589f62a56b64fb
Alex PC Classroom <>
2022-10-21 11:05:06 +0200

    comeme los huevos el que lo lea

---
+++ Exercicio261/src/exercicio261/Exercicio261.java
@@ -0,0 +1,64 @@
+/*
+
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+
+ */
+
+package exercicio261;
+
+
+
+/**
+
+ *
+
+ * Esta es la clase principal.
+
+ * 
+
+ * @author Bilo Alejandro Martins Gonzalez 
+
+ */
+
+public class Exercicio261 {
+
+
+
+    /**
+
+     * Este es el metodo de la clase principal
+
+     * 
+
+     * @param args the command line arguments
+
+     */
+
+    public static void main(String[] args) {
+
+        int x = 0;
+
+        int count = 0;
+
+        while (count < 10) {
+
+            if ((x % 2) != 0) {
+
+                System.out.println(x);
+
+                count++;
+
+            } else {
+
+            }
+
+            x--;
+
+        }
+
+    }
+
+}
+
